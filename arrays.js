34f5e46962dc1170c2ad1ef339808860b459aafc
Twisted7ech <bradcederberg@gmail.com>
2022-10-17 14:56:19 -0600

    Day 17

--- arrays.js
+++ arrays.js
@@ -227,3 +227,27 @@ function shuffleLoop(arr){
 // You're probably feeling pretty good about your shuffle function and I'm betting it's amazing. How long did it take? Your second task today is to google how to shuffle a 2D array and read about why some approaches are better than others. Find a shuffle method that you like. How does it compare in speed, randomness, and thoroughness to yours?
 // My goal with this challenge is to hopefully show you that often finding a method or other code online is not only OK, but a time-saver and sometimes better than what you could've done yourself! If you walk away with that knowledge, I'm a happy camper.
 
+
+//Day 13
+
+// This week has only 6 days of challenges, and this is day 6. Ready to mix it up and put those array functions to good use? I hope so!
+
+// Today you'll be plugging in your functions to a skeleton react project to create a sliding puzzle game. Remember those things? I've added comments to the project Codepen, but before you click over there let's go over some key tasks and notes.
+
+// Here is the Codepen for the project: https://codepen.io/blawblawLaw/pen/xxjMNrN
+
+// You'll see 3 files: html, css, and js already over there. Please do the following:
+
+// Fork the Codepen if it didn't automatically
+// Add a link to your Github or Twitter to the footer in the html file (I know Codepen outside links don't work, but humor me)
+// Insert your daily functions where noted in the js file
+// Note that they need specific names and parameters
+// Remember to insert any noted code from the comment within the functions
+// Play your game! Are there bugs? No worries, debug away
+// This is my first time having people blind-build functions to plug into a project, so if you get stuck feel free to reach out! It could be that I need to modify the instructions.
+
+// I want to point out that I did call it a skeleton project because it is! Feel free to make it your own and add styles or content. Personally, I want to investigate sprites and try that out for the individual tiles so that I can easily use any square image and have a grid size selector.
+
+// Share your forked pen with me @huntoberTweets! I'd love to see what you do with it!
+
+
